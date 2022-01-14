213f957c5cbb893ff874ffebdd7bb8e305c12ec4
GIBBE <valentin.gibbe@epita.fr>
2022-01-14 17:13:56 +0100

    first commit

---
+++ Obra/Obra/Obra/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Obra.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Obra.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
