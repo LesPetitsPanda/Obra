3f53cb944a1f3db4427b0a0c424508286205645d
GIBBE <valentin.gibbe@epita.fr>
2022-02-05 17:04:57 +0100

    professional

--- Obra/Obra/Obra/SQLTools/ProfessionalAccount/ProfessionalAccount.cs
+++ Obra/Obra/Obra/SQLTools/ProfessionalAccount/ProfessionalAccount.cs
@@ -1,7 +1,19 @@
-﻿namespace Obra.SQLTools.Professional
+﻿using MySql.Data.MySqlClient;
+using mySQLConnect;
+
+namespace Obra.SQLTools.Professional
 {
     public class ProfessionalAccount
     {
-        
+        protected MySQLConnectUtility _connect;
+        public static MySqlConnection conn;
+        private string url = "";
+        public ProfessionalAccount(string email, string password, string username, string name, string adresse,
+            string pays, string birth_day)
+        {
+            _connect = new MySQLConnectUtility("");
+            _connect.AddUser(username, password);
+            
+        }
     }
 }
