f669bf8b30a3d7af72f2d39320c1fb4aeb71aba9
alexandra2506 <97701459+alexandra2506@users.noreply.github.com>
2022-02-12 16:24:59 +0100

    Création de pages

---
+++ Obra/Obra/Obra/Views/CalendarPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Obra.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalendarPage 
+    {
+        public CalendarPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Obra/Obra/Obra/Views/MainPagePart.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Obra.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPagePart 
+    {
+        public MainPagePart()
+        {
+            var Settings = new Button { BackgroundColor = Color.Gray, Text = "Settings" };
+        }
+    }
+}
